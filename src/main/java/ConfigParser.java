ac092459b7b3d0373ecb85e0f1d9cc5763dce9bb
obrienu <obrienules@gmail.com>
2020-08-04 14:23:56 +0100

    tweak code to receive file, change file reader

--- src/main/java/ConfigParser.java
+++ src/main/java/ConfigParser.java
@@ -1,93 +1,70 @@
 import java.io.*;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ConfigParser {
     private Map<String, String> config = new HashMap<>();
-    private String production = "./src/main/java/config.txt";
-    private String staging = "./src/main/java/config.txt.staging";
-    private String development = "./src/main/java/config.txt.dev";
-    private String name_of_file;
-    /*The regex splits the string into groups of key and value pair
-     *It further spits each group into two group, group1 = key and group2 = value
-     */
-    private Pattern pattern = Pattern.compile("((?:\\[\\w+\\]\\s)?\\w+(?:-?\\w+)*)=(\\/?(?:\\w+)(?:(?:\\/|-|\\.)?\\w)+)",
-            Pattern.CASE_INSENSITIVE);
-
     /**
      *
      * @param name_of_file
      */
     public ConfigParser(String name_of_file) {
         //dynamically sets the config file based on the name of the file
-        if("staging".equals(name_of_file)) {
-            this.name_of_file = staging;
-        } else if("development".equals(name_of_file)) {
-            this.name_of_file = development;
-        }
-        getConfigInput();
+        getConfigInput(name_of_file);
     }
 
-    public ConfigParser() {
-        //defaults to production in absence of a name of file
-        this.name_of_file = production;
-        getConfigInput();
-    }
 
     /**
      *
      * @return
      */
     // Gets config data from config file and passes the data to setConfigData method
-    public String getConfigInput() {
+    public void getConfigInput(String name_of_file) {
+        Path path = Paths.get(name_of_file);
 
-        StringBuffer configText = new StringBuffer();
-        int data;
-        File configFile = new File(name_of_file);
-
-        try(Reader bufferedInputStream = new BufferedReader( new FileReader(configFile))) {
-            while((data = bufferedInputStream.read())  != -1) {
-                //Build returned data from input file
-                configText.append((char) data);
+        try {
+            List<String> arr_of_onfigText = Files.readAllLines(path);
+            boolean isApplicationData = false;
+            for (String configText : arr_of_onfigText) {
+                if (configText.contains("application")) {
+                    isApplicationData = true;
+                    continue;
+                } else if (configText.trim().equals("")) {
+                    isApplicationData = false;
+                    continue;
+                }
+                setConfigData(configText, isApplicationData);
             }
-            setConfigData(configText.toString());
-
-            return configText.toString();
-        }catch (FileNotFoundException e){
+        }
+        catch(FileNotFoundException e){
             e.getMessage();
         }
-        catch (IOException e) {
-            e.getMessage();
+        catch(IOException e){
+                e.getMessage();
+            }
+        catch(Exception e){
+                e.getMessage();
+            }
         }
-        catch (Exception e) {
-            e.getMessage();
-        }
-        return configText.toString();
-    }
 
     /**
      *
-     * @param str
+     * @param configKeyValuePair
      */
     //Splits config data into key value pair and save them to Map
-    private void setConfigData(String str) {
-        System.out.println(str);
-        String key;
-
-        Matcher matcher = pattern.matcher(str);
-        while(matcher.find()){
-            key = matcher.group(1);
-            //replace spaces in key with '.' and remove square brackets
-            key = key.replaceAll("\\s", ".").replaceAll("(\\[|\\])", "");
-            if (!config.containsKey(key)) {
-                config.put(key, matcher.group(2));
-            }
+    private void setConfigData(String configKeyValuePair, boolean isApplicationData) {
+        String[] arrOfKeyAndValue = configKeyValuePair.split("=");
+        String key = isApplicationData ? "application." + arrOfKeyAndValue[0]
+                : arrOfKeyAndValue[0];
+        if (!config.containsKey(key)) {
+            config.put(key, arrOfKeyAndValue[1]);
         }
     }
+
 
     /**
      *
